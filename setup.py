0ea94a916f50ec09a1eeaf160c62bbfae0616ff1
Alex Jarosch <research@alexj.at>
2019-11-09 11:48:36 +0100

    update setup.py
    Just some fine tuning

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.0.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -100,11 +100,11 @@ setup(
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author='Alex Jarosch',  # Optional
+    author='Alexander H. Jarosch',  # Optional
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='',  # Optional
+    author_email='research@alexj.at',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
